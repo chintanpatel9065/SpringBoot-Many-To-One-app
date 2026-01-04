fb1de4ee672e37a79ea0dc02f8132000c2669da9
chintanpatel <chintanpatel9065@gmail.com>
2026-01-04 16:08:42 +0530

    Initialize Spring Boot project with Maven. Add project structure, dependencies, application configuration, and essential files like `.gitignore` and `.gitattributes`.

---
+++ src/main/java/org/chintanpatel/springbootmanytooneapp/SpringBootManyToOneAppApplication.java
@@ -0,0 +1,13 @@
+package org.chintanpatel.springbootmanytooneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootManyToOneAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootManyToOneAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chintanpatel/springbootmanytooneapp/SpringBootManyToOneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chintanpatel.springbootmanytooneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootManyToOneAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
